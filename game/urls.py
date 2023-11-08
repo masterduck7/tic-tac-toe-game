ab5e46ad31e0937109ef3c82c6bbd63aeff15fa3
Luis Ramirez <lp.ramirez.duarte.7@gmail.com>
2023-11-08 20:25:05 +0100

    chore(game): Change url to play game

--- game/urls.py
+++ game/urls.py
@@ -7,6 +7,6 @@ app_name = "game"
 urlpatterns = [
     path("games/", game.GamesList.as_view()),
     path("games/<str:name>/", game.GameDetail.as_view()),
-    path("play/<str:name>/", game.PlayGameDetail.as_view()),
+    path("games/<str:name>/play/", game.PlayGameDetail.as_view()),
     path("users/<str:username>/", user.UserDetail.as_view()),
 ]
